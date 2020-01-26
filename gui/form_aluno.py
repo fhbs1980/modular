d28bc78617ff064dc6d1c0dc94ece1fcf3f3581d
fhbs1980 <bevilac@inf.puc-rio.br>
2020-01-26 09:14:59 -0300

    refatoracao campos consulta

--- gui/form_aluno.py
+++ gui/form_aluno.py
@@ -15,7 +15,7 @@ def alunos_incluir(root):
 	def insere():
 		
 		matricula = matriculaEntry.get().strip()
-		retorno = insere_aluno(matricula, nomeEntry.get())
+		retorno = insere_aluno(matricula, nomeEntry.get().title())
 		if retorno == 1:
 			apresentaDialogo('Matrícula já cadastrada','Erro')
 		elif retorno == 2:
@@ -43,28 +43,50 @@ def alunos_consultar(root):
 	matriculaEntry = Entry(frame, textvariable="")
 	matriculaEntry.focus_set()
 	
+	textoLabelRespostaMatricula = StringVar()
+	textoValorRespostaMatricula = StringVar()
+	textoLabelRespostaNome = StringVar()
+	textoValorRespostaNome = StringVar()
+	
+	textoLabelRespostaMatricula.set("")
+	textoValorRespostaMatricula.set("")
+	textoLabelRespostaNome.set("")
+	textoValorRespostaNome.set("")
+	
 	def consulta():
 		
 		matricula = matriculaEntry.get().strip()
 		retorno = consulta_aluno(matricula)
 		if retorno == None:
+			textoLabelRespostaMatricula.set("")
+			textoValorRespostaMatricula.set("")
+			textoLabelRespostaNome.set("")
+			textoValorRespostaNome.set("")
+			
 			apresentaDialogo('Matrícula não encontrada','Erro')
-			matriculaEntry.delete(0,END)
+			matriculaEntry.delete(0,END)			
 			matriculaEntry.focus_set()
 		else:
-			respostaMatriculaLabel = Label(frame, text="Matricula:", underline=0)
-			respostaMatriculaValor = Label(frame, text=retorno['matricula'].title())
-			respostaNomeLabel = Label(frame, text="Nome:", underline=0)
-			respostaNomeValor = Label(frame, text=retorno['nome'].title())
-			respostaMatriculaLabel.grid(row=1, column=0, sticky=W, pady=3,padx=3)
-			respostaMatriculaValor.grid(row=1, column=1, columnspan=3, sticky=EW, pady=3, padx=3)
-			respostaNomeLabel.grid(row=2, column=0, sticky=W, pady=3,padx=3)
-			respostaNomeValor.grid(row=2, column=1, columnspan=3, sticky=EW, pady=3, padx=3)
+			textoLabelRespostaMatricula.set("Matricula:")
+			textoValorRespostaMatricula.set(retorno['matricula'])
+			textoLabelRespostaNome.set("Nome:")
+			textoValorRespostaNome.set(retorno['nome'])
 	
 	consultaButton = Button(frame, text="Consultar", command=consulta)
 	matriculaLabel.grid(row=0, column=0, sticky=W, pady=3,padx=3)
 	matriculaEntry.grid(row=0, column=1, columnspan=3, sticky=EW, pady=3, padx=3)
 	consultaButton.grid(row=3, column=2, sticky=EW, pady=3, padx=3)
+	
+	respostaMatriculaLabel = Label(frame, textvariable=textoLabelRespostaMatricula, underline=0)
+	respostaMatriculaValor = Label(frame, textvariable=textoValorRespostaMatricula)
+	respostaMatriculaLabel.grid(row=1, column=0, sticky=W, pady=3,padx=3)
+	respostaMatriculaValor.grid(row=1, column=1, columnspan=3, sticky=EW, pady=3, padx=3)
+	
+	respostaNomeLabel = Label(frame, textvariable=textoLabelRespostaNome, underline=0)
+	respostaNomeValor = Label(frame, textvariable=textoValorRespostaNome)
+	respostaNomeLabel.grid(row=2, column=0, sticky=W, pady=3,padx=3)
+	respostaNomeValor.grid(row=2, column=1, columnspan=3, sticky=EW, pady=3, padx=3)
+	
 	frame.grid(row=0, column=0, sticky=NSEW)
 	frame.columnconfigure(1, weight=1)
 	root.bind('<Key>', lambda a : configura_enter(a,consulta))
